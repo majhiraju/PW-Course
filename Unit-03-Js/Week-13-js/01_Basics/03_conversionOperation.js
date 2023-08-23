53e8da6d8d0a948ebe45def01816dba27c91417e
Raju <manjhiraju1995@gmail.com>
2023-08-23 14:06:54 -0700

    add

---
+++ Unit-03-Js/Week-13-js/01_Basics/03_conversionOperation.js
@@ -0,0 +1,69 @@
+let score = "Raju"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+
+let isLoggedIn = "Raju"
+
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn);
+
+//1 => true; 0 => false
+//"Raju" => true
+
+let someNumber = 33
+
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//********************  Operatiion  ******************** */
+
+let value = 3
+let nagValue = -value
+// console.log(nagValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " raju"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 10
+++gameCounter;
+console.log(gameCounter);
+
+let x = 3
+const y = x++
+
+console.log(y);
