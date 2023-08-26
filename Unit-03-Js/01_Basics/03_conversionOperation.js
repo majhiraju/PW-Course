62f31dd67e2b5ac3acca7cab808eaf93ce0c769a
Raju <manjhiraju1995@gmail.com>
2023-08-26 19:15:10 +0530

    add

--- Unit-03-Js/01_Basics/03_conversionOperation.js
+++ Unit-03-Js/01_Basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
-let score = "Raju"
+let score = "33abc"
 
-// console.log(typeof score);
-// console.log(typeof (score));
+console.log(typeof score);
+console.log(typeof (score));
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
@@ -10,60 +10,20 @@ let valueInNumber = Number(score)
 
 //"33" => 33
 //"33abc" => NaN
-//true => 1; false => 0
+//ture => 1, false => 0
 
+let isLoggedIn = "raju"
 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-let isLoggedIn = "Raju"
+//1 => 1, false => 0
+// "" => false
+ // "raju" => true
 
-let BooleanisLoggedIn = Boolean(isLoggedIn)
-// console.log(BooleanisLoggedIn);
+ let someNumber = 33
 
-//1 => true; 0 => false
-//"Raju" => true
-
-let someNumber = 33
-
-
-let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
-
-
-//********************  Operatiion  ******************** */
-
-let value = 3
-let nagValue = -value
-// console.log(nagValue);
-
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**2);
-// console.log(2/2);
-// console.log(2%2);
-
-let str1 = "hello"
-let str2 = " raju"
-
-let str3 = str1 + str2
-// console.log(str3);
-
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-
-// console.log(+true);
-// console.log(+"");
-
-
-let gameCounter = 10
-++gameCounter;
-console.log(gameCounter);
-
-let x = 3
-const y = x++
-
-console.log(y);
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
